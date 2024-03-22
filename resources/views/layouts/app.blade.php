2f97741c343496b488ffc4e7860de65d06eda3cd
Anthony <93596000+AnthonyHartog@users.noreply.github.com>
2024-03-22 10:23:33 +0100

    tailwind css cdn
    tailwind css cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <script src="https://cdn.tailwindcss.com"></script>
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
